938d6355a7b51e1324b778c0bc6118950f28baed
James R <james@scalablegroup.com>
2018-04-28 08:19:22 -0700

    Update core

--- pages.js
+++ pages.js
@@ -1,7 +1,15 @@
-// Will Must be first 
 // Make sure you add them alphabetically 
 var monsterPages = {
     Cthulhu: "Cthulhu",    
     Dragon: "Dragon",
     Golem: "Golem",
+ };
+
+
+var normalPages = {
+    About: "About",
+    Alonso: "Alonso Solorzano",
+    Comment: "Comment",
+    WillRiter: "Will Riter",
+    Sitemap: "Sitemap",
  };
---
+++ pages/template.js
@@ -0,0 +1,35 @@
+var name = "blank";
+var img = "blank.jpg";
+var meter = "40";
+var description = "blank";
+var lives = "blank";
+var kill = "blank";
+var eats = "blank";
+
+// 
+// OR
+// OR
+// OR
+// 
+
+var name = `
+  blank
+`;
+var img = `
+  blank.jpg
+`;
+var meter = `
+  67
+`;
+var description = `
+  blank
+`;
+var lives = `
+  blank
+`;
+var kill = `
+  blank
+`;
+var eats = `
+  blank
+`;
