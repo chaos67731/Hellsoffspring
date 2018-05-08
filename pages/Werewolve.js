d56fffa6b452447e89b6e37a209c5a4552954f98
willriter <38712634+willriter@users.noreply.github.com>
2018-05-07 18:40:15 -0700

    Got more basics

--- pages/Werewolve.js
+++ pages/Werewolve.js
@@ -5,17 +5,17 @@ var img = `
     blank.jpg
 `;
 var meter = `
-    67
+    75
 `;
 var description = `
-    Werewolves
+    Werewolves are huminoid creatures that are very tall and hairy.
 `;
 var lives = `
-    Werewolves
+    Werewolves monstly live in 
 `;
 var kill = `
     You can kill Werewolves by
 `;
 var eats = `
-    Werewolves Eat only the human heart. No one really knows why they only eat the human heart. but the ones that do kneo whell they didn't live to tell the story.
+    Werewolves Eat only the human heart. No one really knows why they only eat the human heart but the ones that do know whell they didn't live to tell the story.
 `;
