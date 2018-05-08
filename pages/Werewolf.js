db79d2f764f2dd74dd6bea97e31ec4d386f27b98
willriter <38712634+willriter@users.noreply.github.com>
2018-05-07 19:10:20 -0700

    Fixed spelling eror

--- pages/Werewolf.js
+++ pages/Werewolf.js
@@ -11,10 +11,10 @@ var description = `
     <p>Werewolves are huminoid creatures that are very tall and hairy. They are humans and only turn into a Werewolf ant night. The werewolf is a widespread concept in folklore, existing in many diferent places. They were most popular during the medieval period. From the early modern period, Belief in werewolves developed in parallel to the belief in witches, in the course of the Late Middle Ages and the Early Modern period. Like the witchcraft trials as a whole, the trial of supposed werewolves emerged in Switzerland in the early 15th century and spread throughout Europe in the 16th century, and hitting a high point in the 18th century.</p>
 `;
 var lives = `
-  <p>Werewolves are widespread and live through out the worl. they mostly live in Europe. Ince they are normal people they could be your neighboor. It is hard to know where one lives because you will have to follow it durning the night back to its house. Witch is very hard considering that they have great hearing and one wrong step and they could here you. </p>  
+  <p>Werewolves are widespread and live through out the world. they mostly live in Europe. Ince they are normal people they could be your neighboor. It is hard to know where one lives because you will have to follow it durning the night back to its house. Witch is very hard considering that they have great hearing and one wrong step and they could here you. </p>  
 `;
 var kill = `
-    <p>You can kill Werewolves by stabing them in the herst with a silver steak or shooting them in the heart with a silver bullet. In general if you cut one with a silver blade it will hurt it but not kill it. You can coat yourself in silver shavings so if it bites you it will hurt it. but if i bites you you will also turn into a werewolf.</p> 
+    <p>You can kill Werewolves by stabing them in the heart with a silver steak or shooting them in the heart with a silver bullet. In general if you cut one with a silver blade it will hurt it but not kill it. You can coat yourself in silver shavings so if it bites you it will hurt it. but if it bites you will also turn into a werewolf.</p> 
 `;
 var eats = `
     <p>Werewolves Eat only the human heart. No one really knows why they only eat the human heart bu t the ones that do know whell they didn't live to tell the story.</p>
