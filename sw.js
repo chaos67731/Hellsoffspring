166cea8de6ba83169e74882c7eaa1d2bb178ab25
James R <james@scalablegroup.com>
2018-05-14 21:34:11 -0700

    App Update

--- sw.js
+++ sw.js
@@ -1,31 +1,55 @@
-self.addEventListener('fetch', function(event) {
+// self.addEventListener('fetch', function(event) {
+// });
+
+/*
+ *
+ *  Air Horner
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+const version = "0.6.9";
+const cacheName = `hellsOffspring-${version}`;
+self.addEventListener('install', e => {
+  const timeStamp = Date.now();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html?timestamp=${timeStamp}`,
+        // `/styles/main.css?timestamp=${timeStamp}`,
+        // `/scripts/main.min.js?timestamp=${timeStamp}`,
+        // `/scripts/comlink.global.js?timestamp=${timeStamp}`,
+        // `/scripts/messagechanneladapter.global.js?timestamp=${timeStamp}`,
+        // `/sounds/airhorn.mp3?timestamp=${timeStamp}`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-// self.addEventListener('install', function(e) {
-//   e.waitUntil(
-//     caches.open('the-magic-cache').then(function(cache) {
-//       return cache.addAll([
-//       	// Pages
-//         '/',
-//         '/#Dragon',
-//         '/#Golem',
-//         '/#Cthulhu',
-//         // Other
-//         '/manifest.json',
-// 		// Style
-//         '/resources/css/bootstrap.css',
-//         '/resources/css/chosen.min.css',
-//         '/resources/css/site.css',
-// 		// JS
-//         '/resources/js/jquery.js',
-//         '/pages.js',
-//         '/resources/js/chosen.jquery.min.js',
-//         '/resources/js/site.js',
-// 		// Images
-//         '/img/site/background-2.jpg',
-//         '/logo.png',
-//       ]);
-//     })
-//   );
-// });
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
