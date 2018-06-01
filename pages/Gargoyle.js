e2c5919b0f6d9356ed8e3bb94794cba7e4e175ac
willriter <38712634+willriter@users.noreply.github.com>
2018-06-01 16:49:20 -0700

    Basic info for Gagoyle

---
+++ pages/Gargoyle.js
@@ -0,0 +1,24 @@
+var name = `
+  Gargoyle
+`;
+var img = `
+  blank.jpg
+`;
+var meter = `
+  67G
+`;
+var description = `
+    A Gargoyle is a living monster that was made of a stone and brought to life by magic. Theyonly come to life during the night. They come in many varieties: some are full bodied, often with wings; others are simply disembodied that simply watch having only half a body. Gargoyles are not the same as other stone beings, such as the Golems are always in the form of in-human,but the gargoyle only become flesh and bone at night. They stand guard in Gothic style structures, such as Cathedrals and European castles. 
+
+    It has also been said that Gargoyles only come alive to fend off evil, which would explain them being found on churches commonly. So there is a possibility that Gargoyles just look evil and scary to fend off bad intruders. (It is also said in some legends that they are a type of lesser guardian angel that is sometimes often considered to be an abomination due to the fact that they are half angel half demon.) So nobody actually knows if a gargoyle is evil or good. 
+
+`;
+var lives = `
+  blank
+`;
+var kill = `
+  blank
+`;
+var eats = `
+  blank
+`;
