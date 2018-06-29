13d00ca30b6ea61247735da134b56474ed2861dc
willriter <38712634+willriter@users.noreply.github.com>
2018-06-29 08:45:08 -0700

    Grimlin
    added biasick for grimlin

---
+++ pages/Gremlin.js
@@ -0,0 +1,23 @@
+var name = `
+Gremlin
+`;
+var img = `
+Gremlin.jpg
+`;
+var meter = `
+15
+`;
+var description = `
+<p></p>
+`;
+var lives = `
+
+`;
+var kill = `
+<p></p> 
+
+<p></p>
+`;
+var eats = `
+
+`;
