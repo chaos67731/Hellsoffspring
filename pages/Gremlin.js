3ff6a232a0b9e6e058bb4d5ca9b9ab244b42ca06
willriter <38712634+willriter@users.noreply.github.com>
2018-07-14 10:00:08 -0700

    Gremlin full
    I added all info for the gremlin

--- pages/Gremlin.js
+++ pages/Gremlin.js
@@ -8,16 +8,14 @@ var meter = `
 15
 `;
 var description = `
-<p></p>
+<p>Gremlins are fluffy and short if they get wet they will multiply, if you feed them after midnight they turn into a monster So be carful and good luck.</p>
 `;
 var lives = `
-
+The gremlins live in space but came to earth some how.
 `;
 var kill = `
-<p></p> 
-
-<p></p>
+<p>To kill a gremlin you most exspose it bright light.</p> 
 `;
 var eats = `
-
+<p>The gremlins usauly eat junk food, and chinken leges.</p>
 `;
