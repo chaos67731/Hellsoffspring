659e4d662e8449ee8fda2440fdcba1c6fff5415d
willriter <38712634+willriter@users.noreply.github.com>
2018-07-31 12:59:31 -0700

    Satan pages

--- pages/The-Devil.js
+++ pages/The-Devil.js
@@ -7,7 +7,9 @@ var meter = `
 55
 `;
 var description = `
-<p> Satan or the devil means "the enemy" or "the trouble-maker" in Hebrew tradisions, Satan is a figure found in the writings of the Abrahamic religions. Muslims, Christians and Jews believe that he tempts people to do bad things. His role is to question and tempt people's faith. They also say that he was once an angel named Lucifer. In Satanism, Satan is thought to be just a metaphor for what humans want. In Theistic Satanism, Satan is considered to be a good force.</p>
+<p> Satan or the devil means "the enemy" or "the trouble-maker" in Hebrew tradisions, Satan is a figure found in the writings of the Abrahamic religions. Muslims, Christians and Jews believe that he tempts people to do bad things. His role is to question and tempt people's faith. </p>
+
+<p>They also say that he was once an angel named Lucifer. In Satanism, Satan is thought to be just a metaphor for what humans want. In Theistic Satanism, Satan is considered to be a good force.</p>
 `;
 var lives = `
 The devil is the king of hell so he also lives in hell.
