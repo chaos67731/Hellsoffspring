4e4a832d3f7f681ba5e9f7a596b4c20e1366ef34
willriter <38712634+willriter@users.noreply.github.com>
2018-08-07 15:20:35 -0700

    added more contet

--- pages/Gremlin.js
+++ pages/Gremlin.js
@@ -8,7 +8,7 @@ var meter = `
 50
 `;
 var description = `
-<p>Gremlins are taller than the mogwai they are also scaly unlike the mogwai the are violent and they like to make mishtuf To make a gremlin you must have a mogwai first and the you have to feed it after midnight for then it will star the tranformation into a gremlin .</p>
+<p>Gremlins are taller than the mogwai they are also scaly unlike the mogwai the are violent and they like to make mishtuf To make a gremlin you must have a mogwai first and the you have to feed it after midnight for then it will star the tranformation into a gremlin but their are many different types of the gremlin some as a spider some with mohawks ect.</p>
 `;
 var lives = `
 The Mogwai and the grimlen come from space and they are supposedly made by scientist on a far away planet
