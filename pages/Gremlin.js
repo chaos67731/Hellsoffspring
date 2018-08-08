1d096729b0f3d61b612c250c7077916b6dce877e
willriter <38712634+willriter@users.noreply.github.com>
2018-08-07 20:48:22 -0700

    Fixed gramer in Gremlin

--- pages/Gremlin.js
+++ pages/Gremlin.js
@@ -8,16 +8,17 @@ var meter = `
 50
 `;
 var description = `
-<p>Gremlins are taller than the mogwai they are also scaly unlike the mogwai the are violent and they like to make mishtuf To make a gremlin you must have a mogwai first and the you have to feed it after midnight for then it will star the tranformation into a gremlin but their are many different types of the gremlin some as a spider some with mohawks ect.</p>
+<p>Gremlins are taller than the mogwai they are also scaly unlike the mogwai the are violent and they like to make mischief To make a gremlin you must have a Mogwai first and you have to feed it after midnight for then it will star the transformation into a gremlin but there are many different types of the gremlin some as a spider some with mohawks etc.</p>
 `;
 var lives = `
-The Mogwai and the grimlen come from space and they are supposedly made by scientist on a far away planet
+The Mogwai and the gremlin come from space and they are supposedly made by a scientist on a faraway planet
 `;
 
 var kill = `
-<p> To kill a gremlin you most exspose it bright light or fire it melts them into a goo . </p> 
+<p> To kill a gremlin you most expose it bright light or fire it melts them into a goo . </p> 
 `;
 
 var eats = `
-<p> The gremlins usauly eat junk food, and chinken leges. </p>
+<p> The gremlins usually eat junk food, and chicken legs. </p>
 `;
+
