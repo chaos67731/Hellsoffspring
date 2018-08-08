cdcc37e9f73df5874305544e8799a5ff1d392bb4
willriter <38712634+willriter@users.noreply.github.com>
2018-08-07 20:51:28 -0700

    FIxed more grammar

--- pages/Mogwai.js
+++ pages/Mogwai.js
@@ -8,16 +8,17 @@ var meter = `
 15
 `;
 var description = `
-<p>Gremlins are fluffy,short and cute . The Mogwai have certin rules that you must foolow or they will turn into a grimlin witch unlike the mogwai the are taller, more violent and are not so cute Rule 1 is Do not get it wet or it will multiply Rule two Do not feed affter mid night Because it will turn into the gremlin. .</p>
+<p>Gremlins are fluffy,short and cute . The Mogwai have certain rules that you must follow or they will turn into a gremlin witch unlike the mogwai the are taller, more violent and are not so cute Rule 1 is Do not get it wet or it will multiply Rule two Do not feed after midnight Because it will turn into the gremlin. .</p>
 `;
 var lives = `
-The Mogwai and the grimlen come from space and they are supposedly made by scientist on a far away planet
+The Mogwai and the Gremlin come from space and they are supposedly made by a scientist on a faraway planet
 `;
 
 var kill = `
-<p> To kill a gremlin you most exspose it bright light. </p> 
+<p> To kill a Gremlin you most exspose it bright light. </p> 
 `;
 
 var eats = `
 <p> The gremlins usauly eat junk food, and chinken leges. </p>
 `;
+
