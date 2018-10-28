cc117ff951c98dd667dad79602029ed1ca05e9a8
willriter <38712634+willriter@users.noreply.github.com>
2018-10-27 20:56:40 -0700

    Grim-reaper
    full

---
+++ pages/Grim-reaper.js
@@ -0,0 +1,20 @@
+var name = `
+Grim-reaper
+`;
+var img = `
+death.jpg
+`;
+var meter = `
+100
+`;
+var description = `
+<p>Grim Reaper. A figure commonly used to represent death. The Grim Reaper is a skeleton or solemn-looking man carrying a scythe, who cuts off people's lives as though he were harvesting grain.</p>  
+`;
+var lives = `
+The Grim-reaper live between ower world and the spirit world passing souls from the human world to the spirit world either to heaven or hell.
+`;
+var kill = `
+<p>There is only one thing that can kill the grim reaper is his own weapon// his own syth. </p> 
+`;
+var eats = `
+Grim-reaper is an immortal and does not eat anything for it does not affect him if he does not eat anything but on acation he tries human food just for the taste `;
